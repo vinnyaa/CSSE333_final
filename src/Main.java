d87892570ffcf4dab16d7d86af86c7cca5145929
schaffll <schaffll@rose-hulman.edu>
2017-05-05 15:13:41 -0400

    Callable statement working - Login UI

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
-import com.microsoft.sqlserver.jdbc.*;
+import java.util.Scanner;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+
 
 	
 public class Main {  
@@ -8,44 +13,129 @@ public class Main {
 	public static String username = "user=KLVGradebook37;";
 	public static String password = "password=Database37;";
 	public static String SQL = "exec testproc"; // TODO: Test sproc by putting string here
+	private static int userID;
+	private static String userPass;
 	
-	public static void main(String[] args) {  
-		System.out.println("Attempting to connect to KLV_Gradebook");
-		// Create a variable for the connection string.  
-		String connectionUrl = "jdbc:sqlserver://golem.csse.rose-hulman.edu:1433;" +  
-			"databaseName=KLV_Gradebook;"
-			+ username
-			+ password;  
+	public static void main(String[] args) throws SQLException {  
+//		System.out.println("Attempting to connect to KLV_Gradebook");
+//		// Create a variable for the connection string.  
+//		String connectionUrl = "jdbc:sqlserver://golem.csse.rose-hulman.edu:1433;" +  
+//			"databaseName=KLV_Gradebook;"
+//			+ username
+//			+ password;  
+//
+//		// Declare the JDBC objects.  
+//		Connection con = null;  
+//
+//		try {  
+//			// Establish the connection.  
+//			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+//			con = DriverManager.getConnection(connectionUrl);  
+//			System.out.println("connection established");
+//		} 	 
+//
+//		catch (Exception e) {  
+//			e.printStackTrace();  
+//		}  
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		
+//		String username;
+//		String password;
+//		
+//		System.out.println("Log in:");
+//		System.out.println("username: ");
+//		username = input.next();
+//		
+//		System.out.println("password: ");
+//		password = input.next();
+		
+		
+		Gui myGui = new Gui();
+		myGui.okButton().addActionListener(new ActionListener() {
+          @Override
+          public void actionPerformed(ActionEvent event)
+          {
+             userID = myGui.getUserID();
+             userPass = myGui.getPass();
+             
+            Connection myConnection = makeConnection();
+             
+//     		CallableStatement stmt;
+     		
+			//SQL = ("EXEC UserLogin @UserID_1 = " + userID);
+			runStatement(myConnection);
+			
+//				stmt = myConnection.prepareCall("{call UserLogin(?,?)}");
+//				stmt.setInt(1, userID);
+//	    		stmt.setString(2, userPass);
+//	    		stmt.execute();
+			
+	         
+			if (myConnection != null) try { myConnection.close(); } catch(Exception e) {}
+			System.out.println("Connection Terminated");
+    		
+    		
+    	//	runStatement(SQL, con);
+          }
+		});
+		
 
-		// Declare the JDBC objects.  
-		Connection con = null;  
+		
+//		SQL = "exec "
+		
+//		while (myGui.okNotPressed() == true){
+//			System.out.print ("");
+//		}
+//		
+//		String username = myGui.getUser();
+//		String password = myGui.getPass();
+		
+//		System.out.print(username);
+		
+		
 
-		try {  
-			// Establish the connection.  
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
-			con = DriverManager.getConnection(connectionUrl);  
-			System.out.println("connection established");
-		} 	 
+		
+//		try{
+		//SQL = ("EXEC UserLogin @UserID_1 = " + Integer.parseInt(username));
+//		}
+//		catch (Exception e){
+//			System.out.print("here");
+//		}
+		
 
-		catch (Exception e) {  
-			e.printStackTrace();  
-		}  
-		runStatement(SQL, con);
-              
-		if (con != null) try { con.close(); } catch(Exception e) {}
-		System.out.println("Connection Terminated");
+//              
+//		if (con != null) try { con.close(); } catch(Exception e) {}
+//		System.out.println("Connection Terminated");
       
    }
 	   
 	   
    // repeatable function that runs the sql statement represented by the string
    // TODO: create a preprocessing string to secure SQL statement from attacks
-   static void runStatement(String arg, Connection con) {
-	   Statement stmt = null;  
+   static void runStatement(Connection con) {
+	   CallableStatement stmt = null;  
 	   ResultSet rs = null;
 	   try {
-		   stmt = con.createStatement();  
-	       rs = stmt.executeQuery(arg); 
+//		   stmt = con.createStatement();  
+		   	stmt = con.prepareCall("{call UserLogin(?,?)}");
+//		   							ResultSet.TYPE_FORWARD_ONLY,
+		   							//ResultSet.CONCUR_READ_ONLY, userID);
+//		   							ResultSet.CLOSE_CURSORS_AT_COMMIT);
+			stmt.setInt(1, userID);
+			stmt.setString(2, userPass);
+			//stmt.registerOutParameter(3, java.sql.Types.NVARCHAR);
+			System.out.println("anything error");
+			rs = stmt.executeQuery();
+			
+			
+			
+//			System.out.println(rs.getObject(3));
+			//System.out.println(rs.getMetaData());
+//		   
+		   
+	      // rs = stmt.executeQuery(arg); 
 	       if(!(rs == null)) {
 		       int col_label_count = rs.getMetaData().getColumnCount();
 			   for (int i = 1; i <= col_label_count; i++){
@@ -64,7 +154,7 @@ public class Main {
 	   } finally {
 	      if (stmt != null) try { con.close(); } catch(Exception e) {}  
 	      if (rs != null) try { con.close(); } catch(Exception e) {}  
-	      System.out.println("Statement Completed: " + arg);
+	      System.out.println("Statement Completed: ");
 	   }  
    }
    
@@ -72,5 +162,31 @@ public class Main {
    String secure_string(String arg) {
 	   System.out.println("Test for special characters in the string here");
 	   return arg;
+   }
+   
+   public static Connection makeConnection(){
+	   System.out.println("Attempting to connect to KLV_Gradebook");
+		// Create a variable for the connection string.  
+		String connectionUrl = "jdbc:sqlserver://golem.csse.rose-hulman.edu:1433;" +  
+			"databaseName=KLV_Gradebook;"
+			+ username
+			+ password;  
+
+		// Declare the JDBC objects.  
+		Connection con = null;  
+
+		try {  
+			// Establish the connection.  
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+			con = DriverManager.getConnection(connectionUrl);  
+			System.out.println("connection established");
+			return con;
+		} 	 
+
+		catch (Exception e) {  
+			e.printStackTrace();  
+		}  
+		
+		return null;
    }
 }  
